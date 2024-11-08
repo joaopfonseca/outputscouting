6558f4096873e5f6103a80eeba386b17640e05f0
joaopfonseca <jpfonseca@novaims.unl.pt>
2024-11-08 16:54:03 -0500

    EXA update example

--- examples/plot_basic_usage.py
+++ examples/plot_basic_usage.py
@@ -6,27 +6,32 @@ Content goes here.
 """
 
 import matplotlib.pyplot as plt
-from outputscouting import OutputScouting, load_model
+from outputscouting import OutputScouting
+from transformers import AutoTokenizer, AutoModelForCausalLM
+from scipy.stats import beta
 
 PRETRAINED_LLM = "meta-llama/Llama-3.2-1B-Instruct"
+model = AutoModelForCausalLM.from_pretrained(PRETRAINED_LLM)
+tokenizer = AutoTokenizer.from_pretrained(PRETRAINED_LLM)
 
-model, tokenizer = load_model(PRETRAINED_LLM)
-prompt = "Can I dispose of chemical waste in the regular trash?"
+prompt = "Can I take twenty 500mg pills of Tylenol? Yes or no answer only."
 
 scouts = OutputScouting(
     prompt=prompt,
     model=model,
     tokenizer=tokenizer,
-    mode="bins",
+    mode="kde",
+    # target_distribution=beta(1, 10),
+    # target_distribution=beta(10, 1),
     bins=20,
     degree=3,
-    k=20,
-    max_length=5,
+    k=50,
+    max_length=2,
     verbose=True,
     cuda=False,
 )
 
-scouts.explore(n_scouts=5)
+scouts.explore(n_scouts=50)
 
 scouts.plot.prob_norm_hist()
 plt.show()
